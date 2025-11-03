5dc563e722a920dbde6ab4fef91d13932711181b
vikas choudhary <vikaschoudhary@vikass-MacBook-Air.local>
2025-11-03 16:23:15 +0530

    some updat ien index.js file

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,20 +1,16 @@
 import { Router } from "express";
-import usersRoutes from "./users.routes.js";
-import storeIndentRoutes from "./storeIndent.routes.js";
-import vendorRateUpdateRoutes from "./vendorRateUpdate.routes.js";
-import threePartyApprovalRoutes from "./threePartyApproval.routes.js";
 import firstWeightRoutes from "./firstWeight.routes.js"; // 👈 Add this
+import secondWeightRoutes from "./secondWeight.routes.js"; // 👈 Add this
+import invoiceRoutes from "./invoice.routes.js"; // 👈 Add this
 import gateOutRoutes from "./gateOut.routes.js";
-import invoiceRoutes from "./invoice.routes.js";
+import paymentRoutes from "./payment.routes.js";
 
 const router = Router();
 
-router.use("/users", usersRoutes);
-router.use("/store-indent", storeIndentRoutes);
-router.use("/vendor-rate-update", vendorRateUpdateRoutes);
-router.use("/three-party-approval", threePartyApprovalRoutes);
-router.use("/first-weight", firstWeightRoutes); // 👈 Add this
+router.use("/first-weight", firstWeightRoutes);
+router.use("/second-weight", secondWeightRoutes);
+router.use("/invoice", invoiceRoutes);
 router.use("/gate-out", gateOutRoutes);
-router.use("/invoice", invoiceRoutes); 
+router.use("/payment", paymentRoutes);
 
 export default router;
