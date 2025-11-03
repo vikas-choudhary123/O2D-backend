4a1257177f08b56c6290b32467b0d791149e30e1
vikas choudhary <vikaschoudhary@vikass-MacBook-Air.local>
2025-11-03 16:16:29 +0530

    some update

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,6 +4,8 @@ import storeIndentRoutes from "./storeIndent.routes.js";
 import vendorRateUpdateRoutes from "./vendorRateUpdate.routes.js";
 import threePartyApprovalRoutes from "./threePartyApproval.routes.js";
 import firstWeightRoutes from "./firstWeight.routes.js"; // 👈 Add this
+import gateOutRoutes from "./gateOut.routes.js";
+import invoiceRoutes from "./invoice.routes.js";
 
 const router = Router();
 
@@ -12,5 +14,7 @@ router.use("/store-indent", storeIndentRoutes);
 router.use("/vendor-rate-update", vendorRateUpdateRoutes);
 router.use("/three-party-approval", threePartyApprovalRoutes);
 router.use("/first-weight", firstWeightRoutes); // 👈 Add this
+router.use("/gate-out", gateOutRoutes);
+router.use("/invoice", invoiceRoutes); 
 
 export default router;
